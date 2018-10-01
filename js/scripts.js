4d12a5ae9597f145539a519fbfa7dfb1139bf00d
stephen22otieno <stephenombiro22@gmail.com>
2018-10-01 07:39:00 +0300

    add all modified

--- js/scripts.js
+++ js/scripts.js
@@ -13,34 +13,25 @@ function Player(turn) {
   this.playerName;
 }
 
-// checking for 1
 Player.prototype.rollone = function() {
   if (this.roll === 1) {
   this.tempscore = 0;
   alert("Sorry " + this.playerName + ", you rolled a 1! Your turn is over!")
-  // this.changeturn();
+  
   } else {
   this.tempscore += this.roll;
   }
 }
 
-// hold
+
 Player.prototype.hold = function () {
   this.totalscore += this.tempscore;
   this.tempscore = 0;
-  // this.changeturn();
-  alert(this.playerName + ", your turn is over, pass the mouse!");
+
+  alert("Sorry" + this.playerName + ", your turn is over, pass the mouse!");
 }
 
-// // changing turn
-// Player.prototype.changeturn = function () {
-//   if (this.roll ===1) {
-//     this.turn = false;
-//   } else {
-//     this.turn = true;
-//   }
-// }
-// check for 100
+
 Player.prototype.winnerCheck = function () {
   if (this.totalscore >= 100) {
     alert(this.playerName + " You are the winner!");
@@ -48,7 +39,7 @@ Player.prototype.winnerCheck = function () {
 }
 
 Player.prototype.newGame = function () {
-  //debugger;
+  
   this.roll = 0;
   this.tempscore = 0;
   this.totalscore = 0;
